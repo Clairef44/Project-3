d8be49a502bda8ff7167c052f92378757d5d076e
WyattMassey <130611144+WyattMassey@users.noreply.github.com>
2023-04-12 18:14:35 -0500

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,91 @@
+  void getName(){
+    return playerName;
+  }
+  void getFullness(){
+    return playerFullness;
+  }
+  void getGold(){
+    return playerGold;
+  }
+  void getCeramicPot(){
+    return playerCeramicPot;
+  }
+  void getFryingPan(){
+    return playerFryngPan;
+  }
+  void getCauldron(){
+    return playerCauldron;
+  }
+  void getClub(){
+    return playerClub;
+  }
+  void getSpear(){
+    return playerSpear;
+  }
+  void getRapier(){
+    return playerRapier;
+  }
+  void getBattleAxe(){
+    return playerBattleAxe;
+  }
+  void getLongSword(){
+    return playerSword;
+  }
+  void getWeapons(){
+    return playerWeapons;
+  }
+  void getTreasures(){
+    return playerTreasures;
+  }
+  void getArmor(){
+    return playerArmor;
+  }
+  void getIngredients(){
+    return playerIngredients;
+  }
+  void getKeys(){
+    return playerKeys;
+  }
+  string getName(string namein){
+    playerName=namein;
+  }
+  int setFullness(int fullin){
+    playerFullness=fullin;
+  }
+  int setGold(int goldin){
+    playerGold=goldin;
+  }
+  int setCeramicPot(int potin){
+    playerCeramicPot=potin;
+  }
+  int setFryingPan(int panin){
+    playerFryingPan=panin;
+  }
+  int setCauldron(int cauldronin){
+    playerCauldron=cauldronin;
+  }
+  int setClub(int clubin){
+    playerClub=clubin;
+  }
+  int setSpear(int spearin){
+    playerSpear=spearin;
+  }
+  int setRapier(int rapierin){
+    playerRapier=cauldronin;
+  }
+  int setBattleAxe(int axein){
+    playerBattleAxe=axein;
+  }
+  int setLongSword(int swordin){
+    playerLongSword=swordin;
+  }
+  int setArmor(int armorin){
+    playerArmor=armorin;
+  }
+  int setIngredients(int ingredientsin){
+    playerIngredients=ingredientsin;
+  }
+  int setkeys(int keyin){
+    playerkeys=keyin;
+  }
+
