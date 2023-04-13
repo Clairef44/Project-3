427fa039692c7e193cb9261e60a29c47c2e74a51
WyattMassey <130611144+WyattMassey@users.noreply.github.com>
2023-04-12 19:23:58 -0500

    Create misfortune.h

---
+++ misfortune.h
@@ -0,0 +1,18 @@
+class Misfortune{
+  
+  private
+  int remainingPlayers;
+  int turn;
+  
+  public
+  
+  Misfortune();
+  void turns();
+  string robbed();
+  string armor_breaks();
+  string food_poisoning();
+  string locked();
+  
+  
+
+};
