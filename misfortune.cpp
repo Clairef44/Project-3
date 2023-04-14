1f709d40282fe472bb13301fda52ac2ad102b209
Clairef44 <130500080+Clairef44@users.noreply.github.com>
2023-04-13 21:51:31 -0600

    Update misfortune.cpp

--- misfortune.cpp
+++ misfortune.cpp
@@ -1,35 +1,70 @@
-  
-  Misfortune(){
+  #include <iostream>
+  #include "Misfortune.h"
+
+  using namespace std;
+
+
+  Misfortune :: Misfortune(){
    remainingPlayers = 4;
   }
-  void turns(){
+
+  Misfortune :: Misfortune(int remainingPlayers_, int turn_){
+    remainingPlayers = remainingPlayers_;
+    turn = turn_;
+  }
+
+  int Misfortune :: getRemainingPlayers(){
+    return remainingPlayers;
+  }
+
+  int Misfortune :: getTurn(){
+    return turn;
+  }
+
+  void Misfortune :: setRemainingPlayers(int players){
+    remainingPlayers = players;
+  }
+
+  void Misfortune :: setTurn(int turns){
+    turn = turns; 
+  }
+
+  void Misfortune :: turns(){
     turn++;
-    variable = rand()%10;
-    if(variable<4){
+    double variable = rand()%10;
+    if(variable < 4){
       variable = rand()%10;
-      if(0<variable&&variable<3){
+      if(0 < variable && variable < 3){
         robbed();
       }
-      else if(2<variable&&variable<6){
+      else if(2 < variable && variable < 6){
         armor_breaks();
       }
-      else if(5<variable&&variable<9){
+      else if(5 < variable && variable < 9){
         food_poisoning();
-      }else if(variable==9){
+      }else if(variable == 9){
         locked();
       }
     }
   }
-  string robbed(){
+
+
+  string Misfortune :: robbed(){
     //will print out robbed message and decrement party's items notifacation
   }
-  string armor_breaks(){
+
+
+  string Misfortune :: armor_breaks(){
     //will remove 1 armor from party and print notification
   }
-  string food_poisoning(){
+
+
+  string Misfortune :: food_poisoning(){
     //will decrement random team members fullness and print notifacation
   }
-  string locked(){
+
+  
+  string Misfortune :: locked(){
     remainingPlayers--;
     //will print out player locked behind door message
   }
